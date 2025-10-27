a364c20b23328b8cd23cefcde82bdf74eb830f6d
Laluarkan <lalunaufalarkan21@gmail.com>
2025-10-28 00:16:25 +0800

    Fix build.sh: Hapus apt-get install git-lfs (sudah di System Dependencies)

--- build.sh
+++ build.sh
@@ -5,12 +5,6 @@ set -o errexit
 echo "Installing requirements..."
 pip install -r requirements.txt
 
-# ▼▼▼ TAMBAHKAN BLOK INI UNTUK MENGINSTAL GIT-LFS MANUAL ▼▼▼
-echo "Updating package list and installing Git LFS..."
-apt-get update
-apt-get install -y git-lfs
-# ▲▲▲ AKHIR TAMBAHAN ▲▲▲
-
 echo "Setting up Git LFS..."
 git lfs install
 echo "Pulling LFS files (models)..."
