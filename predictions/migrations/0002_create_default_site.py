a40a391c291a8e03312aff89565be4df388ac2f9
Laluarkan <lalunaufalarkan21@gmail.com>
2025-10-28 16:56:07 +0800

    V2: Deploy Awal yang Bersih (Tanpa LFS)

--- predictions/migrations/0002_create_default_site.py
+++ predictions/migrations/0002_create_default_site.py
@@ -1,30 +1,21 @@
 # predictions/migrations/0002_create_default_site.py
-
 from django.db import migrations
 from django.conf import settings
 
-# Ganti 'prediksi-kan-1.onrender.com' dengan URL Render Anda
-APP_DOMAIN = 'prediksi-kan-1.onrender.com' 
+APP_DOMAIN = 'prediksi-kan-1.onrender.com' # Ganti jika nama Render Anda berbeda
 
 def create_site(apps, schema_editor):
     Site = apps.get_model('sites', 'Site')
-    # Ini akan membuat ATAU memperbarui baris id=1
     Site.objects.update_or_create(
         id=settings.SITE_ID, # (SITE_ID = 1)
-        defaults={
-            'domain': APP_DOMAIN,
-            'name': 'Prediksi-Kan'
-        }
+        defaults={'domain': APP_DOMAIN, 'name': 'Prediksi-Kan'}
     )
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        # Gantungkan pada migrasi pertama (0001) DAN migrasi 'sites'
-        ('predictions', '0001_initial'), 
-        ('sites', '0002_alter_domain_unique'), 
+        ('predictions', '0001_initial'), # Bergantung pada migrasi pertama Anda
+        ('sites', '0002_alter_domain_unique'), # Bergantung pada migrasi 'sites'
     ]
-
     operations = [
         migrations.RunPython(create_site),
     ]
