cd536046ab494b4bd52c2604028544680ae79347
Laluarkan <lalunaufalarkan21@gmail.com>
2025-11-01 00:33:28 +0800

    hapus file test.py yang berisi API key

--- test.py
+++
@@ -1,8 +0,0 @@
-
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBzDYkSQ9oQiC9jvwW5_v5bKewBtCSx68o")
-
-# Tampilkan model yang tersedia
-for m in genai.list_models():
-    print(m.name)
