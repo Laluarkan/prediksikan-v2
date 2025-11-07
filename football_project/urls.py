d48b74ab0b7ad33368c7fc103f81a616ab712a8a
Laluarkan <lalunaufalarkan21@gmail.com>
2025-11-08 07:20:02 +0800

    atur ADS

--- football_project/urls.py
+++ football_project/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
 
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('ads.txt', TemplateView.as_view(template_name="ads.txt", content_type="text/plain")),
 
 ]
